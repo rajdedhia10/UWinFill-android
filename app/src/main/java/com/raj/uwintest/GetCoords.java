1b7b9ab03d51d9561c8bda0c1fcfcddef95c7a61
Raj Dedhia <rajdedhia10@gmail.com>
2022-06-29 23:52:51 -0400

    Location Check Initial

---
+++ app/src/main/java/com/raj/uwintest/GetCoords.java
@@ -0,0 +1,59 @@
+package com.raj.uwintest;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.GoogleMap;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+public class GetCoords extends AsyncTask<Void,Void,Void> {
+    String result;
+    private GoogleMap googleMap;
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+
+        try {
+            Log.i("gETcOORDS", "TRYING TO CONNECT");
+            URL url = new URL("https://pastebin.com/raw/vDYs5ehu");
+            Log.i("gETcOORDS", "CONNECTED TO URL");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            Log.i("gETcOORDS", "CONNECTED TO NETWORK");
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while (line != null) {
+                line = bufferedReader.readLine();
+//                result = result + line;
+                List<String> coordList = Arrays.asList(line.split(","));
+                String latitude = coordList.get(0);
+                String longitude = coordList.get(1);
+                double str1 = Double.parseDouble(latitude);
+                double str2 = Double.parseDouble(longitude);
+                LatLng current = new LatLng(str1, str2);
+                googleMap.addMarker(new MarkerOptions()
+                        .position(current));
+//                        .title("Current Location"));
+            }
+        } catch (MalformedURLException e) {
+//            Log.i("myApp", "CATCH MALFORMED URL");
+            e.printStackTrace();
+        } catch (IOException e) {
+//            Log.i("myApp", "IO EXCEPTION");
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/com/raj/uwintest/MapsActivity.java
@@ -0,0 +1,117 @@
+package com.raj.uwintest;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.raj.uwintest.databinding.ActivityMapsBinding;
+
+import java.text.DecimalFormat;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
+    private ActivityMapsBinding binding;
+
+    @SuppressLint({"WrongThread", "StaticFieldLeak"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        if (ContextCompat.checkSelfPermission(MapsActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MapsActivity.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)){
+                ActivityCompat.requestPermissions(MapsActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }else{
+                ActivityCompat.requestPermissions(MapsActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+
+
+//        new Thread(new GetCoords(){
+//            public void run() {
+//                GetCoords process = new GetCoords();
+//                process.doInBackground();
+//            }
+//        }).start();
+
+//        new GetCoords().execute(doInBackground());
+
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    public double[] getLocation() {
+        GpsTracker gpsTracker = new GpsTracker(MapsActivity.this);
+        double[] myList = new double[0];
+        if (gpsTracker.canGetLocation()) {
+            double latitude = gpsTracker.getLatitude();
+            double longitude = gpsTracker.getLongitude();
+            myList = new double[2];
+            DecimalFormat df = new DecimalFormat("0.0000000");
+            myList[0] = Double.parseDouble(df.format(latitude));
+            myList[1] = Double.parseDouble(df.format(longitude));
+            Toast.makeText(this, String.valueOf(myList[0]), Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, String.valueOf(myList[1]), Toast.LENGTH_SHORT).show();
+        } else {
+            gpsTracker.showSettingsAlert();
+        }
+        return myList;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+//        LatLng sydney = new LatLng(-33.852, 151.211);
+        double[] returned = getLocation();
+        LatLng current = new LatLng(returned[0], returned[1]);
+        googleMap.addMarker(new MarkerOptions()
+                .position(current)
+                .title("Current Location"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(current));
+
+        float zoomLevel = 16.0f; //This goes up to 21
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(current, zoomLevel));
+
+
+//        GetCoords process = new GetCoords();
+//        process.doInBackground();
+//        googleMap.addMarker(new MarkerOptions()
+//                .position(sydney)
+//                .title("Marker in Sydney"));
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+}
